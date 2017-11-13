829c5f6bfa99be0ae12b7c8ab27f4b97cc0ad846
Meike Steinleger <Meike.steinleger@t-online.de>
2017-11-13 13:47:18 +0100

    Add Access-Control-Allow-Origin

--- tasks/serve.js
+++ tasks/serve.js
@@ -77,8 +77,38 @@ module.exports = function(grunt) {
 				path: process.cwd()
 			}
 		});
-		
+		app.use(function (req, res, next) {
+			
+				// Website you wish to allow to connect
+				res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/#/');
+			
+				// Request methods you wish to allow
+				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+			
+				// Request headers you wish to allow
+				res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+			
+				// Set to true if you need the website to include cookies in the requests sent
+				// to the API (e.g. in case you use sessions)
+				res.setHeader('Access-Control-Allow-Credentials', true);
+			
+				// Pass to next layer of middleware
+				next();
+			});
+
 		app.get ('/', function(req, res) {
+			// Website you wish to allow to connect
+				res.setHeader('Access-Control-Allow-Origin', '*');
+			
+				// Request methods you wish to allow
+				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+			
+				// Request headers you wish to allow
+				res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+			
+				// Set to true if you need the website to include cookies in the requests sent
+				// to the API (e.g. in case you use sessions)
+				res.setHeader('Access-Control-Allow-Credentials', true);
 			try{
 				var cert = fs.readFileSync(options.keypath);
 				var token = req.headers.webtoken;
