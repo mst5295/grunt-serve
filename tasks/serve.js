f2f1fd13095da64db5316ae986d7c5750baef92f
Meike Steinleger <Meike.steinleger@t-online.de>
2017-11-13 13:51:06 +0100

    change Access-Control-Allow-Origin

--- tasks/serve.js
+++ tasks/serve.js
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
 		app.use(function (req, res, next) {
 			
 				// Website you wish to allow to connect
-				res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/#/');
+				res.setHeader('Access-Control-Allow-Origin', '*');
 			
 				// Request methods you wish to allow
 				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -97,18 +97,6 @@ module.exports = function(grunt) {
 			});
 
 		app.get ('/', function(req, res) {
-			// Website you wish to allow to connect
-				res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/#/');
-			
-				// Request methods you wish to allow
-				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-			
-				// Request headers you wish to allow
-				res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-			
-				// Set to true if you need the website to include cookies in the requests sent
-				// to the API (e.g. in case you use sessions)
-				res.setHeader('Access-Control-Allow-Credentials', true);
 			try{
 				var cert = fs.readFileSync(options.keypath);
 				var token = req.headers.webtoken;
