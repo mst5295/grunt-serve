4148beaadbf0a3d1f598f6aad2110100548ae4b4
Meike Steinleger <Meike.steinleger@t-online.de>
2017-11-13 13:48:58 +0100

    change Access-Control-Allow-Origin

--- tasks/serve.js
+++ tasks/serve.js
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
 
 		app.get ('/', function(req, res) {
 			// Website you wish to allow to connect
-				res.setHeader('Access-Control-Allow-Origin', '*');
+				res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/#/');
 			
 				// Request methods you wish to allow
 				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
