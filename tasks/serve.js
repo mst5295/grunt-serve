85a512f211631d43e21ec273c1b0ebe37f9a2c25
Meike Steinleger <Meike.steinleger@t-online.de>
2017-11-21 12:31:49 +0100

    delete header taskinfo in cors

--- tasks/serve.js
+++ tasks/serve.js
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
 				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 			
 				// Request headers you wish to allow
-				res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,webtoken,taskinfo');
+				res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,webtoken');
 			
 				// Set to true if you need the website to include cookies in the requests sent
 				// to the API (e.g. in case you use sessions)
